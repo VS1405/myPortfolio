efe2d2d38c6334cbd6eb32c1baabec8f4b692d8f
Varsha Mhaske <varshamhaske808@gmail.com>
2024-03-06 21:20:43 -0800

    new proj added

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -3,7 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import calculator from '../assets/img/calculator.png'
 import medicalApp from '../assets/img/medical.png'
 import restaurantApp from '../assets/img/restaurant.png';
-
+import Ecommerce from '../assets/img/e-commerce.png'
 import colorSharp2 from '../assets/img/color-sharp2.png'
 
 export const Projects = () => {
@@ -27,6 +27,14 @@ export const Projects = () => {
       link: `https://restaurant-app-ashy.vercel.app/`
     }
   ];
+  const project2 = [
+    {
+      title: "E-commerce App",
+      imgUrl: Ecommerce,
+      desc: "Created e-commerce using React.JS, JS, Tailwind",
+      link: `https://e-commerce-website-ivory.vercel.app/`
+    },
+  ]
   return (
     <section id="projects">
       <Container className="project">
@@ -54,8 +62,16 @@ export const Projects = () => {
                     })}
                   </Row>
                 </Tab.Pane>
-                <Tab.Pane eventKey="second">Empty</Tab.Pane>
-                <Tab.Pane eventKey="third">Empty</Tab.Pane>
+                <Tab.Pane eventKey="second">
+                <Row>
+                    {project2.map((project, i) => {
+                      return <ProjectCard key={i} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <h3 style={{display: 'flex', justifyContent: "center"}}>Empty</h3>
+                </Tab.Pane>
               </Tab.Content>
             </Tab.Container>
           </Col>
